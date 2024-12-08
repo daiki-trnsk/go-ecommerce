f1a883aff37f27c927df7f04ea264d67af25385a
daiki <dahuizhusong@gmail.com>
2024-12-08 22:30:01 +0900

    12/8

---
+++ controllers/addres.go
@@ -0,0 +1,24 @@
+package controllers
+
+import(
+
+)
+
+func AddAddress() gin.HandlerFunc{
+
+}
+
+func EditHomeAddress() gin.HandlerFunc{
+
+}
+
+func EditWorkAddress() gin.HandlerFunc{
+
+}
+
+func DeleteAddress() gin.HandlerFunc{
+
+}
+
+
+
--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"os"
+
+	"github.com/daiki-trnsk/go-ecommerce/controllers"
+	"github.com/daiki-trnsk/go-ecommerce/database"
+	"github.com/daiki-trnsk/go-ecommerce/middleware"
+	"github.com/daiki-trnsk/go-ecommerce/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	port :=os.Getenv("PORT")
+	if port == ""{
+		port = "8000"
+	}
+
+	app :=controllers.NewApplication(database.Products(database.Client, "Products"), database.UserData(database.Client, "Users"))
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitem", app.RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
+}
